e08c3313e375edbd28d86487ed699376e23b70a3
Fokhrul Islam <shahidcse12@gmail.com>
2022-04-05 02:49:20 +0600

    charts added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,24 +4,24 @@ const Blog = () => {
     return (
         <div className='container'>
             <h1 className='text-3xl font-bold'>Question and Answer</h1>
-            <p class='mt-10 mb-3 font-semibold text-gray-900'>
+            <p className='mt-10 mb-3 font-semibold text-gray-900'>
                 What are the Semantic Tags?
             </p>
-            <p class='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
+            <p className='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
                 HTML5 semantic tags define the purpose of the element. By using semantic markup, you help the browser understand the meaning of the content instead of just displaying it. By providing this extra level of clarity, HTML5 semantic elements also help search engines to read the page and find the required information faster..
             </p>
 
-            <p class='mt-10 mb-3 font-semibold text-gray-900'>
+            <p className='mt-10 mb-3 font-semibold text-gray-900'>
                 What is Context API?
             </p>
-            <p class='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
+            <p className='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
                 Context API is a React API that can solve a lot of problems that modern applications face related to state management and how they’re passing state to their components. Instead of installing a state management library in your project that will eventually cost your project performance and increase your bundle size, you can easily go with Context API and be fine with it.
             </p>
 
-            <p class='mt-10 mb-3 font-semibold text-gray-900'>
+            <p className='mt-10 mb-3 font-semibold text-gray-900'>
                 Difference Between Inline / Inline-Block and Blocks
             </p>
-            <p class='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
+            <p className='text-gray-600 w-[500px] m-auto border-2 p-2 shadow-lg'>
                 inline The element does not start on a new line and only occupy just the width it requires. You can not set the width or height.
 
                 Inline-block Its formatted just like the inline element, where it does not start on a new line. BUT, you can set width and height values.
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Bar, BarChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import  { PureComponent } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
-    const data =[
+    const data = [
         {
             "month": "Mar",
             "investment": 100000,
@@ -41,11 +42,12 @@ const Dashboard = () => {
         }
     ]
     return (
-        <div className='grid llg:grid-cols-2'>
-           
+        <div>
 
-            <div className='px-40 mt-10'>
-                <h4 className='text-2xl font-mono font-bold text-purple-700 mb-10' >Investment vs Revenue</h4>
+            <h4 className='text-2xl font-mono font-bold text-purple-700 mb-10' >Investment vs Revenue</h4>
+
+
+            <div className='grid grid-cols-2 mx-12 pb-5 mt-20 gap-10'>
                 <LineChart width={400} height={300} data={data}>
 
                     <Line dataKey={"sell"}></Line>
@@ -56,18 +58,50 @@ const Dashboard = () => {
 
                 </LineChart>
 
-                <div className='flex items-center mt-20'>
-                
-        <BarChart width={400} height={300} data={data}>
-          <Bar dataKey="sell" fill="#8884d8" />
-          <XAxis dataKey={"month"}></XAxis>
-                    <YAxis dataKey={"sell"}></YAxis>
+
+
+                <BarChart width={400} height={300} data={data}>
+                <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+          <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                    <XAxis dataKey={"month"}></XAxis>
+                    <YAxis dataKey={"investment"}></YAxis>
                     <Tooltip></Tooltip>
                     <Legend></Legend>
-        </BarChart>
-     
-                </div>
+                </BarChart>
+
+
+                <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" />
+          <YAxis />
+          <Tooltip />
+          <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+        </AreaChart>
+
+
+        <PieChart width={400} height={400}>
+          <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+          <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+          <Tooltip></Tooltip>
+          <Legend></Legend>
+        </PieChart>
+
+
             </div>
+
+
+
         </div>
     );
 };
